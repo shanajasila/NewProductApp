14b0bfbcd58d19b9b63bdd83aa3ef0676534175b
shanajasila <shanajasilam@gmail.com>
2022-12-06 22:11:29 +0530

    viewproduct

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
+ 
+  id=""
+  productname=""
+  productcode=""
+  mfgdate=""
+  expdate=""
+  brand=""
+  price=""
+  sellername=""
+  distributorname=""
 
+  readValues=()=>{
+    let data={"id":this.id,"productname":this.productname,"productcode":this.productcode,"mfgdate":this.mfgdate,"expdate":this.expdate,"brand":this.brand,"price":this.price,"sellername":this.sellername,"distributorname":this.distributorname}
+    console.log(data)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
